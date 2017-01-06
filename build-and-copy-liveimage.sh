fcce5025218185e5783b05171ef86c156ae4e94f
Olivier Renault <monoliv@gmail.com>
2017-01-06 15:21:19 -0800

    Finish script to build / based upon kickstart

--- build-and-copy-liveimage.sh
+++ build-and-copy-liveimage.sh
@@ -3,12 +3,29 @@
 # Generate a livecd 
 # Generate pxe image 
 # Copy initrd and image into /var/lib/tftpboot/livecd/
+PXEDIR="/var/lib/tftpboot"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR/build
-if [ -d /var/lib/tftpboot/livecd]:
-	rm -Rf /var/lib/tftpboot/livecd
+if [ ! -d "$DIR/build" ]; then
+	mkdir "$DIR/build"
 fi
-rm -Rf tftpboot
 
-livecd-creator -c kickstart/centos-7-live-base.ks --releasever=7 -f centos7-livecd
-mv 
+cd "$DIR/build"
+
+# Clean env
+if [ -d "$PXEDIR/livecd" ]; then
+	rm -Rf "$PXEDIR/livecd"
+	mkdir "$PXEDIR/livecd"
+fi
+if [ -d "$DIR/build/tftpboot" ]; then
+	rm -Rf "$DIR/build/tftpboot"
+fi
+
+# Generate livecd
+livecd-creator -c $DIR/kickstart/centos-7-live-base.ks --releasever=7 -f centos7-livecd
+
+# Extract initrd / vmlinuz
+livecd-iso-to-pxeboot centos7-livecd.iso
+
+# Copy in place
+cp -ax "$DIR/build/tftpboot/vmlinuz0" "$PXEDIR/livecd/"
+cp -ax "$DIR/build/tftpboot/initrd0.img" "$PXEDIR/livecd/"
